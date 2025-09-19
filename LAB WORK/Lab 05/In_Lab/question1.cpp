77de8dcd73956d021ba5a62afe4cfcb3934598fa
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-09-19 12:01:01 +0500

    Update question1.cpp

--- LAB WORK/Lab 05/In_Lab/question1.cpp
+++ LAB WORK/Lab 05/In_Lab/question1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+int calFactorial(int n){
+	
+	if (n==1){
+		return n;
+	}
+	
+	return n=n*calFactorial(n-1);
+}
+
+
+
+int main(){
+	int num;
+	cout <<"Enter number: ";
+	cin >>num;
+	
+	int res =calFactorial(num);
+	cout <<"Factorial for "<<num<<" is: "<<res;
+}
+
+
 
