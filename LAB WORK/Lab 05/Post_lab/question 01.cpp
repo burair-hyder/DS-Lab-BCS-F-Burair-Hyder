e6c2a2aac1ab5a08e29e258a75300920f6ee4dd7
Syed Muhammad  Burair Hyder <burairhyder519@gmail.com>
2025-10-02 16:02:28 +0500

    Update question 01.cpp

--- LAB WORK/Lab 05/Post_lab/question 01.cpp
+++ LAB WORK/Lab 05/Post_lab/question 01.cpp
@@ -1,1 +1,25 @@
-p
+#include <iostream>
+using namespace std;
+
+
+int calFac(int x){
+	
+	if (x==1){
+		return x;
+	}
+	
+	if (x==0){
+		return 1;
+	}
+	
+	return x*calFac(x-1);
+}
+
+int main(){
+	int value;
+	cout <<"Enter Value For Factorial: ";
+	cin >> value;
+	int y=  calFac(value);
+	cout <<"The Factorial For Value "<<value <<" is : "<<y<<endl;
+	
+}
